71d547c9387d079078187080c9043a8c2ae06a44
Soua Xiong <68033651+sxiong6901@users.noreply.github.com>
2020-09-28 19:47:20 -0400

    Update employee.js

--- Develop/template-engine-employee-summary-master/lib/employee.js
+++ Develop/template-engine-employee-summary-master/lib/employee.js
@@ -1,21 +1,18 @@
 class Employee {
-    constructor (name, id, email) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
+    constructor(name, id, email) {
+      if (!name || !name.trim().length) {
+        throw new Error("Expected parameter 'name' to be a non-empty string");
+      }
+      if (typeof id !== "number" || isNaN(id) || id < 0) {
+        throw new Error("Expected parameter 'id' to be a non-negative number");
+      }
+      if (!email || !email.trim().length) {
+        throw new Error("Expected parameter 'name' to be a non-empty string");
+      }
+      this.name = name;
+      this.id = id;
+      this.email = email;
     }
-    getName() {
-        return this.name;
-    }
-    getId() {
-        return this.id;
-    }
-    getEmail(){
-        return this.email;
-    }
-    getRole() {
-        return 'Employee';
-    }
-}
-
-module.exports = Employee
+  }
+  
+  module.exports = Employee;
